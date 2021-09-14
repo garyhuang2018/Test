3f255d1af6bad1a8e34ce80a63d6c0dfa6e06079
garyGemvary <gary.huang@gemvary.com>
2021-09-14 19:08:32 +0800

    add QMessage when success

--- src/local_ota.py
+++ src/local_ota.py
@@ -9,18 +9,20 @@ from PyQt5 import QtCore, QtWidgets
 
 
 class Ui(object):
+
     def setup(self, Form):
         Form.setObjectName("From")
-        Form.resize(416, 48)
+        Form.resize(520, 48)
         self.textEdit = QtWidgets.QTextEdit(Form)
-        self.textEdit.setGeometry(QtCore.QRect(10, 10, 301, 27))
+        self.textEdit.setGeometry(QtCore.QRect(10, 10, 451, 27))
         self.textEdit.setAcceptDrops(True)
         self.textEdit.setStyleSheet("font-family:微软雅黑;")
         self.textEdit.setObjectName("textEdit")
+        self.textEdit.setToolTip("将应用文件拖入后升级")
         self.textEdit.textChanged.connect(self.editchange)
         self.textEdit.setReadOnly(False)
         self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(320, 9, 75, 28))
+        self.pushButton.setGeometry(QtCore.QRect(520, 9, 75, 28))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.open)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -31,9 +33,15 @@ class Ui(object):
             _apk_path = self.textEdit.toPlainText().replace('file:///', '').replace("/", "\\")
             self.textEdit.setText(self.textEdit.toPlainText().replace('file:///', '').replace("/", "\\"))
             print('apk安装路径:', _apk_path)
-            QMessageBox.information(None, "", str(subprocess.Popen("adb devices", shell=True)))
-        #    subprocess.Popen("adb install -r -d " + _apk_path, shell=True)
-        #    os.system('adb install -r -d ' + _apk_path)
+            with os.popen("adb install -r -d " + _apk_path, "r") as p:
+                r = p.read()
+            result = r.find("Success")
+            message_widget = QtWidgets.QWidget()
+            if result == -1:
+                QMessageBox.about(message_widget, "提示", "安装失败")
+            else:
+                QMessageBox.about(message_widget, "提示", "安装成功")
+            self.textEdit.clear()
 
     def open(self):
         if os.path.exists(self.textEdit.toPlainText()):
@@ -43,7 +51,7 @@ class Ui(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "打开"))
+        self.pushButton.setText(_translate("Form", "升级"))
 
 
 if __name__ == '__main__':
@@ -52,6 +60,7 @@ if __name__ == '__main__':
     form = QtWidgets.QWidget()
     ui = Ui()
     ui.setup(form)
+    form.setWindowTitle("应用升级")
     form.show()
     # 系统exit()方法确保应用程序干净的退出
     # 的exec_()方法有下划线。因为执行是一个Python关键词。因此，exec_()代替
