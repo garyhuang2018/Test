e3216376d23d5ca21f62c89722dcd28b4e55ea55
gary <gary.huang@gemvary.com>
2022-07-23 13:55:04 +0800

    add indoor auto answering, fetch the ip address of the indoor first and answer the call

--- src/design/start.py
+++ src/design/start.py
@@ -3,29 +3,39 @@
 import os
 from time import sleep
 
+import requests
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from test_tool import Ui_MainWindow
 import sys
+import logging
+import uiautomator2 as u2
+
+# 门口机设备列表接口
+DEV_LIST = 'http://ip:8080/v3/devlist'
 
 
 class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.setupUi(self)
-        self.ip_address_input.setText("192.192.255.79")
+        self.ip_address_input.setText("192.192.10.52")
         self.port_input.setText('5555')
         self.times_input.setText('10')
-        self.room_no_input.setText('0104')
+        self.room_no_input.setText('0501')
         self.my_thread = MyThread()
         self.call_button.clicked.connect(lambda: self.start_call_thread())
         self.my_thread.signal.connect(self.callback)  # 设置任务线程发射信号触发的函数
+        self.textBrowser.setReadOnly(True)
 
     def start_call_thread(self):
         """
         启动多线程
         :return:
         """
+        logging.warning('start calling thread')
+        self.textBrowser.append('start calling')
         try:
             outdoor_address = self.ip_address_input.text() + ":" + self.port_input.text()
             self.my_thread.outdoor_address = outdoor_address
@@ -41,6 +51,7 @@ class MyWindow(QMainWindow, Ui_MainWindow):
             print(e)
 
     def callback(self, i):  # 这里的 i 就是任务线程传回的数据
+        self.textBrowser.append('finish calling')
         os.system('adb disconnect')
         QMessageBox.about(self, "循环呼叫提示", i)
 
@@ -54,15 +65,44 @@ class MyThread(QThread):
         :return:
         """
         for i in range(0, self.times):
-            print('calling ')
             cmd = 'adb -s ' + self.outdoor_address + ' shell input keyevent STAR'
+            os.system(cmd)
             os.system(cmd)
             cmd = 'adb -s ' + self.outdoor_address + ' shell input text ' + self.room_no
             os.system(cmd)
             cmd = 'adb -s ' + self.outdoor_address + ' shell input keyevent POUND'
             os.system(cmd)
-            sleep(20)
+            self.answer_room_no_match()
+            sleep(25)
         self.signal.emit("呼叫完成")  # 发出任务完成信号
+
+    def answer_room_no_match(self):
+        """
+        : match room info， 找出室内机IP
+        """
+        url = DEV_LIST.replace('ip', self.outdoor_address.split(':')[0])  # 将接口中的ip替换获取接口数据
+        dev_list = requests.get(url).json().get("data")
+        floor_no = self.room_no[0:2]  # 切割前面2个数字作为楼层，后面2个数字作为房号
+        room_no = self.room_no[2:]
+        indoor_ip = ""
+        for i in range(0, len(dev_list)):
+            if dev_list[i].get('floorNo') == floor_no and dev_list[i].get('roomNo') == room_no:
+                indoor_ip = str(dev_list[i].get('ipAddr'))
+                print(dev_list[i].get('ipAddr'))
+        adb_device = u2.connect(indoor_ip + ":5555")
+        print(adb_device.info)
+        adb_device(text='接听').click()
+
+
+class QTextEditLogger(logging.Handler):
+    def __init__(self, parent):
+        super().__init__()
+        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget.setReadOnly(True)
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.appendPlainText(msg)
 
 
 if __name__ == '__main__':
