1d6bbfddf8dbdcf46994f54bdf9aadfe4e024594
gary <gary.huang@gemvary.com>
2023-06-19 20:16:12 +0800

    requests from digital indoor

--- src/design/test/test_edge_gateway.py
+++ src/design/test/test_edge_gateway.py
@@ -8,20 +8,36 @@ from locust import HttpUser, between, task
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 6)
+    wait_time = between(1, 8)
 
-    @task(4)
-    def index(self):
+    @task
+    def get_unit_status(self):
+        self.client.get("/dev/unit-status?zoneCode=5588")
+
+    @task
+    def get_house_struct(self):
         self.client.get("/dev/housing-struct-list?zoneCode=5588&page=0&size=9999")
 
-    @task(3)
-    def search(self):
+    @task
+    def get_notice_list(self):
         self.client.get("/dev/notice-list?zoneCode=5588&page=0&size=30&unitNo=010102")
 
-    @task(2)
+    @task
     def device_config(self):
         self.client.get("/dev/device-config?zoneCode=5588")
 
     @task
     def zone_authorizes(self):
-        self.client.get("/dev/zone-authorizes?zoneCode = 5588 & devCode = a684ee954489 & devType = 14")
+        self.client.get("/dev/zone-authorizes?zoneCode=5588&devCode=a684ee954489&devType=14")
+
+    @task
+    def dev_reg(self):
+        self.client.post("/dev/dev-reg?zoneCode=5588")
+
+    @task
+    def sync_time(self):
+        self.client.post("/dev/sync-time?zoneCode=5588")
+
+    @task
+    def get_manager(self):
+        self.client.post("/dev/dev-list?zoneCode=5588&page=0&size=30&devTypes=1")
