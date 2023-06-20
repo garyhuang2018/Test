2a94047821168da42bcbfda6c25384ac58180e83
gary <gary.huang@gemvary.com>
2023-06-20 17:08:28 +0800

    add interface

--- src/design/test/test_edge_gateway.py
+++ src/design/test/test_edge_gateway.py
@@ -8,7 +8,7 @@ from locust import HttpUser, between, task
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 8)
+    wait_time = between(0, 1)
 
     @task
     def get_unit_status(self):
@@ -36,8 +36,21 @@ class WebsiteUser(HttpUser):
 
     @task
     def sync_time(self):
-        self.client.post("/dev/sync-time?zoneCode=5588")
+        self.client.get("/dev/sync-time?zoneCode=5588")
 
     @task
     def get_manager(self):
-        self.client.post("/dev/dev-list?zoneCode=5588&page=0&size=30&devTypes=1")
+        self.client.get("/dev/dev-list?zoneCode=5588&page=0&size=30&devTypes=1")
+
+    @task
+    def get_data_version(self):
+        self.client.get("/dev/data-version?zoneCode=5588&version=2")
+
+    @task
+    def get_indoor_upgrade(self):
+        self.client.get("/dev/upgrade-check?zoneCode=5588&model=64&versionCode=60023053&packageName=com.gemvary.indoor.launcher&devCode=240079914514341d004f")
+
+    @task
+    def get_launcher_upgrade(self):
+        self.client.get(
+            "/dev/upgrade-check?zoneCode=5588&model=64&versionCode=61023152&packageName=com.gemvary.indoor&devCode=240079914514341d004f")
