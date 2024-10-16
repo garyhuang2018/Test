432f845408754100398326a690a9426febe253d6
gary <gary.huang@gemvary.com>
2024-10-16 19:36:28 +0800

    select project and change gui for factory tool

--- src/hotel/app_action.py
+++ src/hotel/app_action.py
@@ -19,18 +19,99 @@ class App:
 
     def start_app(self):
         subprocess.run('adb shell am start com.gemvary.vhpsmarthome/.ui.main.LauncherActivity')
+        subprocess.run('adb shell input keyevent BACK')
+
+    def add_gateway(self):
+        self.click_element_if_resource_exists("com.gemvary.vhpsmarthome:id/tv_gateway_name")
+        # self.device(resourceId="com.gemvary.vhpsmarthome:id/tv_gateway_name", text="添加新网关").click()
+
+    def choose_project_name(self, hotel_name):
+        self.device(resourceId="com.gemvary.vhpsmarthome:id/id_treenode_label", text=hotel_name).click()
+        self.device(resourceId="com.gemvary.vhpsmarthome:id/tv_gateway_name", text="无网关").click()
+
+    def load_rooms(self):
+        self.device(resourceId="com.gemvary.vhpsmarthome:id/fresh").click()
+        room_names = []
+        # 使用-d参数导出logcat日志
+        process = subprocess.Popen(['adb', 'logcat', '-d', '-s', 'wx'], stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()  # 获取输出
+        if stderr:
+            print(f"Error: {stderr.decode('utf-8', errors='ignore')}")  # 错误输出
+
+        # 解析logcat输出
+        for line in stdout.decode('utf-8', errors='ignore').splitlines():
+            # 查找所有"hotelName"的实例
+            start_index = 0
+            while True:
+                start_index = line.find('"floorAndRoomNumber":"', start_index)
+                if start_index == -1:
+                    break
+                start_index += len('"floorAndRoomNumber":"')
+                end_index = line.find('"', start_index)
+                if end_index != -1:
+                    room_name = line[start_index:end_index]
+                    room_names.append(room_name)
+                start_index = end_index
+        print(room_names)
+        return room_names
 
     def load_project_name(self):
-        if self.device.xpath('//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_project_name"]/android.widget.ImageView[1]').exists:
-            self.device.xpath('//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_project_name"]/android.widget.ImageView[1]').click()
-            self.device(resourceId="com.gemvary.vhpsmarthome:id/fresh").click()
+        # 点击首页
+        self.click_element_if_exists('//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_home"]/android.widget.ImageView[1]')
+        if self.device.xpath(
+                '//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_project_name"]/android.widget.ImageView[1]').exists:
+            self.device.xpath(
+                '//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_project_name"]/android.widget.ImageView[1]').click()
 
+        self.device(resourceId="com.gemvary.vhpsmarthome:id/fresh").click()
+        sleep(1)
+        hotel_names = []
+        # 使用-d参数导出logcat日志
+        process = subprocess.Popen(['adb', 'logcat', '-d', '-s', 'wx'], stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()  # 获取输出
+        if stderr:
+            print(f"Error: {stderr.decode('utf-8', errors='ignore')}")  # 错误输出
 
-def unlock(device):
-    # 亮屏
-    device.unlock()
-    # 等待屏幕亮起
-    sleep(1)  # 等待 1 秒钟以确保屏幕完全亮起
+        # 解析logcat输出
+        for line in stdout.decode('utf-8', errors='ignore').splitlines():
+            # 查找所有"hotelName"的实例
+            start_index = 0
+            while True:
+                start_index = line.find('"hotelName":"', start_index)
+                if start_index == -1:
+                    break
+                start_index += len('"hotelName":"')
+                end_index = line.find('"', start_index)
+                if end_index != -1:
+                    hotel_name = line[start_index:end_index]
+                    hotel_names.append(hotel_name)
+                start_index = end_index
+        print(hotel_names)
+        return hotel_names
+
+    def unlock(self):
+        # 亮屏
+        self.device.unlock()
+        # 等待屏幕亮起
+        sleep(1)  # 等待 1 秒钟以确保屏幕完全亮起
+
+    def click_element_if_resource_exists(self, resource_Id):
+        # 检查元素是否存在
+        if self.device(resourceId=resource_Id):
+            self.device(resourceId=resource_Id).click()
+            print(f"Clicked on element with resourceId: {resource_Id}")
+        else:
+            print(f"Element with xpath: {resource_Id} does not exist.")
+
+    def click_element_if_exists(self, xpath):
+        # 检查元素是否存在
+        if self.device.xpath(xpath).exists:
+            self.device.xpath(xpath).click()
+            print(f"Clicked on element with xpath: {xpath}")
+        else:
+            print(f"Element with xpath: {xpath} does not exist.")
 
 
 def device_locate(device):
@@ -86,10 +167,16 @@ def get_logcat_logs():
 
 
 if __name__ == "__main__":
-    device = ui.connect()
-    print(device.info)
-    unlock(device)
+    d = App()
+    d.load_rooms()
+    # d.add_gateway()
+    # d.unlock()
+    # print(d.load_project_name())
+    # d.start_app()
+    # d.load_project_name()
+    # d.click_element_if_exists('//*[@resource-id="com.gemvary.vhpsmarthome:id/ll_home"]/android.widget.ImageView[1]')
+    # d.choose_project_name('蓝牙测试酒店')
     # device_locate(device)
     # sleep(4)
     # delete_gateway(device)
-    get_logcat_logs()
+    # get_logcat_logs()
--- src/hotel/run.py
+++ src/hotel/run.py
@@ -1,7 +1,7 @@
 # encoding= utf-8
 # __author__= gary
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QMessageBox, QComboBox
 from hotel.app_action import App
 
 
@@ -18,11 +18,15 @@ class FactoryToolApp(QMainWindow):
         self.app_action = App()
         self.current_step = 0
         self.result_table.setColumnWidth(0, 250)
-        self.result_table.setColumnWidth(1, 40)
+        self.result_table.setColumnWidth(1, 200)
+        self.project_names = []
 
         self.pass_button.clicked.connect(self.pass_clicked)
         self.back_button.clicked.connect(self.back_clicked)
         self.update_nav_buttons()
+        self.run_button.clicked.connect(self.run_clicked)
+
+    def run_clicked(self):
         self.print_first_page_result()
 
     def print_first_page_result(self):
@@ -34,8 +38,9 @@ class FactoryToolApp(QMainWindow):
         # 检查手机连接
         if self.app_action.device_available:
             msg = "连接手机成功,型号：" + self.app_action.get_info().get("productName")
-            self.result_table.setItem(row_count, 0, QTableWidgetItem(msg))
-            self.result_table.setItem(row_count, 1, QTableWidgetItem('√'))
+            self.result_table.setItem(row_count, 0, QTableWidgetItem("连接手机，打开usb调试"))
+            self.result_table.setItem(row_count, 1, QTableWidgetItem(msg))
+            self.result_table.setItem(row_count, 2, QTableWidgetItem('√'))
         else:
             # 创建提示框
             msg = QMessageBox()
@@ -46,7 +51,61 @@ class FactoryToolApp(QMainWindow):
             # 显示提示框并等待用户关闭
             msg.exec_()
         self.app_action.start_app()
-        self.app_action.load_project_name()
+        self.app_action.unlock()
+        # 添加新行
+        self.result_table.insertRow(row_count + 1)
+        self.project_names = self.app_action.load_project_name()
+        if self.project_names is not None:
+            # 在最后一列添加下拉框
+            combo_box = QComboBox()
+            # 连接信号和槽
+            for project in self.project_names:
+                combo_box.addItem(project)
+            self.result_table.setItem(row_count + 1, 0, QTableWidgetItem("从手机获取项目，请选择需要测试的项目"))
+            self.result_table.setCellWidget(row_count + 1, 1, combo_box)
+            self.result_table.setItem(row_count + 1, 2, QTableWidgetItem('√'))
+            combo_box.currentIndexChanged.connect(self.status_changed)
+
+    def add_row(self) -> int:
+        # 获取当前行数
+        row_count = self.result_table.rowCount()
+        # 添加新行
+        self.result_table.insertRow(row_count)
+        return row_count
+
+    def status_changed(self, index):
+        # 获取选中的项目名称
+        selected_project = self.project_names[index]
+        #
+        # 创建确认对话框
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Question)
+        msg_box.setText(f"您选择的项目是：{selected_project}")
+        msg_box.setWindowTitle("确认选择")
+        msg_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        #
+        # 显示对话框并获取用户响应
+        response = msg_box.exec_()
+
+        if response == QMessageBox.Ok:
+            print(f"用户确认选择项目：{selected_project}")
+            # 在此处添加确认后的操作
+            self.app_action.choose_project_name(selected_project)
+            # row_count = self.add_row()
+            # new_combo_box = QComboBox()
+            # new_combo_box.addItem("项目有网关")
+            # new_combo_box.addItem("项目无网关")
+            # self.result_table.setItem(row_count, 0, QTableWidgetItem("请选择项目是否有网关"))
+            # self.result_table.setCellWidget(row_count, 1, new_combo_box)
+            # self.result_table.setItem(row_count, 2, QTableWidgetItem('√'))
+            # new_combo_box.currentIndexChanged.connect(self.gateway_exists)
+        else:
+            print("用户取消选择")
+
+    def gateway_exists(self, index):
+        # "添加新网关", "无网关"
+        if index == 0:
+            self.app_action.add_gateway()
 
     def update_nav_buttons(self):
         for i, button in enumerate(self.nav_buttons):
