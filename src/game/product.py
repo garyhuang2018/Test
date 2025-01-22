a78f5b65b7b438fc68fcaae40ed0906f82e9e3e3
gary <gary.huang@gemvary.com>
2025-01-22 17:20:35 +0800

    Decoupling: By separating the database logic into the Product class

---
+++ src/game/product.py
@@ -0,0 +1,45 @@
+# encoding= utf-8
+# __author__= gary
+import sys
+import json
+import requests
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
+import pymysql
+
+
+class Product:
+    def __init__(self, model, name, record):
+        self.model = model
+        self.name = name
+        self.record = record
+
+    @staticmethod
+    def insert_product(model, name, record):
+        try:
+            connection = pymysql.connect(
+                host='192.192.10.10',
+                port=3306,
+                user='root',  # 数据库用户名
+                passwd='root',  # 密码
+                charset='utf8',
+                db='test_tool'
+            )
+            
+            cursor = connection.cursor()
+            insert_query = "INSERT INTO records (model, name, record) VALUES (%s, %s, %s)"
+            cursor.execute(insert_query, (model, name, record))
+            connection.commit()
+        
+        except pymysql.MySQLError as err:
+            print(f"Database Error: {err}")
+        
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+
+
+if __name__ == "__main__":
+    # product = Product("CS-86TG", "调光开关", "还在测试中")
+    Product.insert_product("CS-86TG", "调光开关", "testing")
